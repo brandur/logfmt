7585660707f4028c265f38c3f9976c83749d85d7
Brandur <brandur@mutelight.org>
2014-12-14 21:20:09 -0800

    Checkpoint towards a single buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 #[deriving(PartialEq)]
 #[deriving(Show)]
-pub struct Pair {
-    pub key: String,
-    pub val: Option<String>,
+pub struct Pair<'a> {
+    pub key: &'a str,
+    pub val: Option<&'a str>,
 }
 
-fn complete_pair(buf: String, pair: Option<Pair>) -> Pair {
-    match pair {
+pub struct PairCollection<'a> {
+    buf: String,
+    pub pairs: Vec<Pair<'a>>,
+}
+
+fn complete_pair<'a>(buf: &'a str, pair: &Option<Pair<'a>>) -> Pair<'a> {
+    match *pair {
         Some(Pair { key: k, val: _ }) =>
             Pair { key: k, val: Some(buf) },
         None =>
@@ -14,34 +19,37 @@ fn complete_pair(buf: String, pair: Option<Pair>) -> Pair {
     }
 }
 
-pub fn parse(message: &str) -> Vec<Pair> {
+pub fn parse(message: &str) -> PairCollection {
     let mut pair: Option<Pair> = None;
     let mut pairs: Vec<Pair> = vec![];
-    let mut buf = String::new();
+    let mut buf = String::with_capacity(message.len());
+    let mut buf_pos = 0u;
 
     let mut escape = false;
     let mut garbage = false;
     let mut quoted = false;
 
-    for c in message.chars() {
+    for (i, c) in message.char_indices() {
         match (quoted, c) {
             (false, ' ') => {
-                if !buf.is_empty() {
+                let slice = buf.as_slice().slice_from(buf_pos);
+                if !slice.is_empty() {
                     if !garbage {
                         // the buffer that we just processed is either a value
                         // or a valueless key depending on the current state of
                         // `pair`
-                        pairs.push(complete_pair(buf, pair));
+                        pairs.push(complete_pair(slice, &pair));
                         pair = None;
                     }
-                    buf = String::new();
+                    buf_pos += slice.len();
                 }
                 garbage = false;
             },
             (false, '=') => {
-                if !buf.is_empty() {
-                    pair = Some(Pair{key: buf, val: None});
-                    buf = String::new();
+                let slice = buf.as_slice().slice_from(buf_pos);
+                if !slice.is_empty() {
+                    pair = Some(Pair { key: slice, val: None });
+                    buf_pos += slice.len();
                 } else {
                     garbage = true;
                 }
@@ -73,8 +81,13 @@ pub fn parse(message: &str) -> Vec<Pair> {
     // and process one final time at the end of the message to get the last
     // data point
     if !garbage {
-        pairs.push(complete_pair(buf, pair));
+        let slice = buf.as_slice().slice_from(buf_pos);
+        pairs.push(complete_pair(slice, &pair));
     }
 
-    pairs
+    // trim any excess capacity off the buffer (discrepancies being caused by
+    // quotes and escape characters)
+    buf.shrink_to_fit();
+
+    PairCollection { buf: buf, pairs: pairs }
 }
